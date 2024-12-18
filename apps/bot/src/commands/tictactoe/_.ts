833651678cd439d38f47f7fa99d6ba3067efb26e
MARCROCK22 <marcos22dev@gmail.com>
2024-12-17 22:17:27 -0400

    remove contexts

--- apps/bot/src/commands/tictactoe/_.ts
+++ apps/bot/src/commands/tictactoe/_.ts
@@ -3,8 +3,6 @@ import { AutoLoad, Command, Declare } from 'seyfert';
 @Declare({
     name: 'tictactoe',
     description: 'a command',
-    integrationTypes: ['GuildInstall', 'UserInstall'],
-    contexts: ['Guild', 'PrivateChannel'],
 })
 @AutoLoad()
 export default class TicTacToe extends Command {}
